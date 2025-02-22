5fd862a24ba807f3f5b14e2b2bc75065e27708c3
Siva Prakash Adari <adari.sivaprakash@gmail.com>
2025-02-22 06:59:10 +0000

    ROMAN

--- Array-String/intToRoman.cpp
+++ Array-String/intToRoman.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<vector>
 
+using namespace std;
+
+string intToRoman(int num) {
+    vector<int> values =   {1000, 900, 500, 400, 100,   90, 50,   40,   10,  9 , 5  , 4,   1};
+    vector<string> romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X","IX", "V", "IV", "I" };
+    string roman_num = "";
+
+    for (int i =0 ; i < values.size(); i++) {
+        while(num >= values[i]) {
+            num = num - values[i];
+            roman_num += romans[i];
+        }
+    }
+
+    return roman_num;
+  
+}
+
+int main() 
+{ 
+   int num;
+   while(getchar() != 'z' ) {
+     cout << "Enter the number :" ;
+     cin >> num;
+     cout << intToRoman(num) << endl;
+   }
+
+}
