a7bacd8db38e802c48293a8c9ae5efeebdfbd783
Siva Prakash Adari <adari.sivaprakash@gmail.com>
2025-05-02 00:10:50 +0000

    Sort

--- Sorting/ComparisonSort/01_selection_sort.cpp
+++ Sorting/ComparisonSort/01_selection_sort.cpp
@@ -8,18 +8,31 @@ public:
    void SelectionSort(vector<int>& nums) {
         for(int i =0; i < nums.size()-1; i++) {
             int minIndex = i;
-            for(int j = i + 1; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
                 if(nums[j] < nums[minIndex]) {
                       minIndex = j;
                 }
             }
-            swap(arr[i],arr[minIndex]);
+            swap(nums[i],nums[minIndex]);
         }
    }
-}
+
+   void print(vector<int>&  nums) {
+      cout << "Elements in vector : "; 
+      for(int i=0; i < nums.size(); i++) {
+          cout << nums[i] << " ";
+      }
+      cout << endl;
+   }
+};
 
 int main() {
   vector<int> arr = {8,5,3,2,1,7,9,4,10,6};
-  SelectionSort(arr);
+  ComparisonSort sort;
+  
+  sort.print(arr);
+  sort.SelectionSort(arr);
+  cout << "After Selection sort " << endl;
+  sort.print(arr);
   return 0;
 }
