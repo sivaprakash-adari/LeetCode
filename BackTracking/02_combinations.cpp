1608c151c3e159d8523537ce8e7b1da26072ebf1
Siva Prakash Adari <adari.sivaprakash@gmail.com>
2025-05-02 00:48:48 +0000

    backtracking

--- BackTracking/02_combinations.cpp
+++ BackTracking/02_combinations.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<vector>
 
+using namespace std;
+
+void combinations(vector<int> curr, int i,vector<int>& nums, vector<vector<int>>& subsets) {
+    if (i > nums.size()) 
+        return;
+    
+    subsets.push_back(curr);
+    for(int j=i; j < nums.size(); j++) {
+        curr.push_back(nums[j]);
+        combinations(curr, j+1, nums,subsets);
+        curr.pop_back();
+    }
+   
+}
+
+int main()
+{
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> subsets;
+    vector<int> curr = {};
+    combinations(curr, 0, nums,subsets);
+    for(int i = 0; i < subsets.size(); i++) {
+        cout << "[" ;
+        for(int j =0; j < subsets[i].size();j++) {
+            cout << subsets[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+}
